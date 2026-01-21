dc769476cea17995020591789a2d0f5ce43b8c7a
Maher Al Rifai <maher.refae15@gmail.com>
2026-01-21 23:45:31 +0200

    create controller and lifecycle for applied jobs page

--- src/api/application/controllers/application.js
+++ src/api/application/controllers/application.js
@@ -1,9 +1,25 @@
+// src/api/application/controllers/application.js
 'use strict';
-
-/**
- * application controller
- */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::application.application');
+module.exports = createCoreController('api::application.application', ({ strapi }) => ({
+  async find(ctx) {
+    // 1. Check if the request is from a website user
+    if (ctx.state.auth.strategy.name === 'users-permissions') {
+      const userId = ctx.state.user.id;
+
+      // 2. Add the user filter to the query
+      // This only affects the Application table, not the populated Jobs/Companies
+      ctx.query.filters = {
+        ...ctx.query.filters,
+        user: userId
+      };
+    }
+
+    // 3. Call the standard Strapi logic with our new filter
+    const { data, meta } = await super.find(ctx);
+
+    return { data, meta };
+  },
+}));
